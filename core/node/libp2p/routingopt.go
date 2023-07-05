a029afb6166e22d458d5784eaf80334653c9e892
jhertz <jh.furtado@campus.fct.unl.pt>
2023-07-05 14:07:54 +0100

    changed bucket-size limit

--- core/node/libp2p/routingopt.go
+++ core/node/libp2p/routingopt.go
@@ -118,7 +118,7 @@ func constructDHTRouting(mode dht.ModeOpt) func(
 				dht.Mode(mode),
 				dht.Datastore(dstore),
 				dht.Validator(validator),
-				dht.BucketSize(5), // set bucket size :)
+				dht.BucketSize(10), // set bucket size :)
 				dht.ProtocolPrefix("/ipfs-exp"),
 			),
 			dual.WanDHTOption(dht.BootstrapPeers(bootstrapPeers...)),
